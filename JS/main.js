3d18bfcb7a46ff1ec01188657cd0d92dddf8a528
fpiattoni <fpiattoni@obralapiedad.com.ar>
2026-05-29 09:36:06 -0300

    Modificacion JS
    Se creo las funciones para aleatorizar la película
    Se creo la función que elimina de la lista la película elegida

--- JS/main.js
+++ JS/main.js
@@ -1,71 +1,16 @@
 const portada= document.getElementById("portada")
 let peliculaData = [];
+let posicionElegida = "";
 const listaPeliculas = [
   "Batman",
   "Better Call Saul",
   "Hellraiser",
   "Inception",
-  "The Matrix",
-  "Interstellar",
-  "Gladiator",
-  "Titanic",
-  "Avatar",
-  "The Godfather",
-  "Pulp Fiction",
-  "Fight Club",
-  "The Dark Knight",
-  "Forrest Gump",
-  "The Shawshank Redemption",
-  "Joker",
-  "The Silence of the Lambs",
-  "Se7en",
-  "Whiplash",
-  "The Prestige",
-  "Django Unchained",
-  "The Departed",
-  "Parasite",
-  "Alien",
-  "Aliens",
-  "Predator",
-  "Terminator 2: Judgment Day",
-  "Back to the Future",
-  "Jurassic Park",
-  "The Lion King",
-  "Toy Story",
-  "Finding Nemo",
-  "Shrek",
-  "Mad Max: Fury Road",
-  "John Wick",
-  "The Avengers",
-  "Iron Man",
-  "Doctor Strange",
-  "Spider-Man",
-  "Logan",
-  "Deadpool",
-  "The Conjuring",
-  "Insidious",
-  "Hereditary",
-  "Midsommar",
-  "It",
-  "Scream",
-  "Halloween",
-  "A Nightmare on Elm Street",
-  "The Exorcist",
-  "Saw",
-  "Blade Runner",
-  "Blade Runner 2049",
-  "Arrival",
-  "The Green Mile",
-  "Cast Away",
-  "Goodfellas",
-  "Casino",
-  "Scarface",
-  "Heat",
-  "a silent place"
+
 ];
 
-
-fetch (`https://www.omdbapi.com/?apikey=a22fb87a&t=${listaPeliculas[getRandomInt(0, listaPeliculas.length-1)]}`)
+//Hay poner el fetch en una funcion que se llame cuando se inicie la pagina y cuando pase a la siguiente pelicula
+fetch (`https://www.omdbapi.com/?apikey=a22fb87a&t=${elegirPelicula()}`)
     .then(response => response.json())
     .then(data => {
         peliculaData = data;
@@ -75,10 +20,18 @@ fetch (`https://www.omdbapi.com/?apikey=a22fb87a&t=${listaPeliculas[getRandomInt
 
 function mostrarPortada(info){
     portada.style.backgroundImage=`url(${info.Poster})`;
+    listaPeliculas
 }
 
-function getRandomInt(min, max) {
-  min = Math.ceil(min);
-  max = Math.floor(max);
-  return Math.floor(Math.random() * (max - min + 1)) + min;
+function eliminarPeliculaPorPosicion(lista, posicion) {
+  lista.splice(posicion, 1); 
+  return lista;
+}
+
+console.log(listaPeliculas);
+
+function elegirPelicula(){
+  const posicionElegida = listaPeliculas[Math.floor(Math.random() * listaPeliculas.length)];
+  eliminarPeliculaPorPosicion(listaPeliculas, listaPeliculas.indexOf(posicionElegida));
+  return posicionElegida;
 }
