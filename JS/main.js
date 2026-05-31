d6336c7a7c2a9732bb85dfca0c37d0872db81c4e
fpiattoni <fpiattoni@obralapiedad.com.ar>
2026-05-31 01:30:30 -0300

    Cambios en main.js
    . Pasé el array de listaPeliculas de nuevo al code main
    . Cree una variable "duracion" en ms (1s = 1000ms")
    . Cree la función que hacer crecer a la timerBar de size 0 a 1 en {duracion} de tiempo 
    
    Notas: La funcion del tiempo ya está hecha, solo queda vincularla con el resto de funciones (boton de siguiente, coldown, fin del tiemo, etc)

--- JS/main.js
+++ JS/main.js
@@ -1,7 +1,72 @@
+
 const portada= document.getElementById("portada")
 let peliculaData = [];
 let posicionElegida = "";
-import { listaPeliculas } from './array.js';
+let duracion = '10000'
+
+const listaPeliculas = [
+  "Batman",
+  "Better Call Saul",
+  "Hellraiser",
+  "Inception",
+  "The Matrix",
+  "Interstellar",
+  "Gladiator",
+  "Titanic",
+  "Avatar",
+  "The Godfather",
+  "Pulp Fiction",
+  "Fight Club",
+  "The Dark Knight",
+  "Forrest Gump",
+  "The Shawshank Redemption",
+  "Joker",
+  "The Silence of the Lambs",
+  "Se7en",
+  "Whiplash",
+  "The Prestige",
+  "Django Unchained",
+  "The Departed",
+  "Parasite",
+  "Alien",
+  "Aliens",
+  "Predator",
+  "Terminator 2: Judgment Day",
+  "Back to the Future",
+  "Jurassic Park",
+  "The Lion King",
+  "Toy Story",
+  "Finding Nemo",
+  "Shrek",
+  "Mad Max: Fury Road",
+  "John Wick",
+  "The Avengers",
+  "Iron Man",
+  "Doctor Strange",
+  "Spider-Man",
+  "Logan",
+  "Deadpool",
+  "The Conjuring",
+  "Insidious",
+  "Hereditary",
+  "Midsommar",
+  "It",
+  "Scream",
+  "Halloween",
+  "A Nightmare on Elm Street",
+  "The Exorcist",
+  "Saw",
+  "Blade Runner",
+  "Blade Runner 2049",
+  "Arrival",
+  "The Green Mile",
+  "Cast Away",
+  "Goodfellas",
+  "Casino",
+  "Scarface",
+  "Heat",
+  "a silent place"
+];
 
 function fetchPelicula (){
   fetch (`https://www.omdbapi.com/?apikey=a22fb87a&t=${elegirPelicula()}`)
@@ -30,4 +95,19 @@ function elegirPelicula(){
   return posicionElegida;
 }
 
-document.addEventListener("DOMContentLoaded", fetchPelicula());
+
+function iniciarTimer() {
+  const timerBar=document.getElementById('temp')
+
+  setTimeout(() => {
+  timerBar.style.animation = 'crecer '+duracion+'ms linear forwards';
+  setTimeout(() => timerBar.remove(), duracion );
+    }, 1);
+  
+  tiempoAgotado()
+}
+
+document.addEventListener("DOMContentLoaded", function(){
+  fetchPelicula() //funcion para llamar a la api
+  iniciarTimer() //Funcion para inciiar el timer
+})
