d6acfb7f75bcd6a051b33a4df5d623b332f4a929
fpiattoni <fpiattoni@obralapiedad.com.ar>
2026-06-21 23:09:17 -0300

    Cambios en el home.js
    Agregue una nueva pantalla y el boton. El "windows.location.href" cambia la pagina cuando le das al boton de jugar

--- JS/home.js
+++ JS/home.js
@@ -1,13 +1,23 @@
 const btnJugar=document.getElementById("JUGAR");
-const btnSiguiente=document.getElementById("Siguiente");
+const btnSiguiente=document.getElementById("Siguiente1");
+const btnSiguiente2=document.getElementById("Siguiente2");
+const btnComenzar=document.getElementById("btnComenzar");
 const screen01=document.getElementById("Screen01");
 const screen02=document.getElementById("Screen02");
+const screen03=document.getElementById("Screen03");
 
+btnComenzar.addEventListener('click', ()=>{
+    window.location.href = "./PAGES/gamePage.html";
+
+});
 btnJugar.addEventListener('click', ()=>{
     MostrarScreen()
 });
 btnSiguiente.addEventListener('click',()=>{
     CambiarScreen()
+})
+btnSiguiente2.addEventListener('click',()=>{
+    CambiarScreen2()
 })
 function MostrarScreen(){
     screen01.classList.add("mostrarTuto")
@@ -16,4 +26,10 @@ function CambiarScreen(){
     screen01.classList.remove("mostrarTuto")
     screen01.classList.add("ocultarTuto")
     screen02.classList.add("mostrarTuto")
+}
+
+function CambiarScreen2(){
+    screen02.classList.remove("mostrarTuto")
+    screen02.classList.add("ocultarTuto")
+    screen03.classList.add("mostrarTuto")
 }
